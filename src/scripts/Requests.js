791b39c9c7a30c24b248332b224448f5f9fcc152
Emma Vogelmeier <emmameiervogel@gmail.com>
2021-06-26 10:14:27 -0500

    final

---
+++ src/scripts/Requests.js
@@ -0,0 +1,33 @@
+import { getRequests, deleteRequest } from "./dataAccess.js"
+
+export const Requests = () => {    
+    const requests = getRequests()
+
+    let html = '<ul>'
+
+    html += requests.map(request => {
+        return `
+            <li> 
+                ${request.description}
+                <button class="request__delete"
+                    id="request--${request.id}">
+                    Delete
+                </button>
+             </li>`
+            
+    }
+    ).join('') 
+
+    html += '</ul>'
+
+    return html
+}
+
+const mainContainer = document.querySelector("#container")
+
+mainContainer.addEventListener("click", click => {
+    if (click.target.id.startsWith("request--")) {
+        const [,requestId] = click.target.id.split("--")
+        deleteRequest(parseInt(requestId))
+    }
+})
